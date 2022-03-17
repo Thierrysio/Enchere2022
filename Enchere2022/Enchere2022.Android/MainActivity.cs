902978e35b848be5f0688f43e0618415d2957563
thierry le gall <thierry28220@live.fr>
2022-03-17 09:00:40 +0100

    mise a jour commentaire mainactivity

--- Enchere2022/Enchere2022.Android/MainActivity.cs
+++ Enchere2022/Enchere2022.Android/MainActivity.cs
@@ -13,11 +13,13 @@ namespace Enchere2022.Droid
     [Activity(Label = "Enchere2022", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        //Ajouter ce code pour gerer les images
         internal static MainActivity Instance { get; private set; }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            //Ajouter ce code pour gerer les images (1 ligne)
             Instance = this;
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
@@ -29,11 +31,11 @@ namespace Enchere2022.Droid
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-
+        //Ajouter ce code pour gerer les images
         public static readonly int PickImageId = 1000;
-
+        //Ajouter ce code pour gerer les images
         public TaskCompletionSource<Stream> PickImageTaskCompletionSource { set; get; }
-
+        //Ajouter cette methode pour gerer les images
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
         {
             base.OnActivityResult(requestCode, resultCode, intent);
