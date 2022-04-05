d3a7d29159a17de0683e0f114f5b78f29f897db3
thierry le gall <thierry28220@live.fr>
2022-04-05 11:19:18 +0200

    ok

--- Enchere2022/Enchere2022/Vues/PageEnchereFlashVue.xaml.cs
+++ Enchere2022/Enchere2022/Vues/PageEnchereFlashVue.xaml.cs
@@ -25,39 +25,48 @@ namespace Enchere2022.Vues
             Grid grid = new Grid
             {
 
-                RowSpacing = 1,
-                ColumnSpacing = 1,
+                RowSpacing = 0,
+                ColumnSpacing = 0,
                 RowDefinitions =
             {
-                new RowDefinition(),
-                new RowDefinition(),
-                new RowDefinition(),
-                new RowDefinition()
+                new RowDefinition  { Height = new GridLength(80) },
+                new RowDefinition  { Height = new GridLength(80) },
+                new RowDefinition  { Height = new GridLength(80) },
+                new RowDefinition  { Height = new GridLength(80) }
             },
                 ColumnDefinitions =
             {
-                new ColumnDefinition(),
-                new ColumnDefinition(),
-                new ColumnDefinition(),
-                new ColumnDefinition()
+                new ColumnDefinition { Width = new GridLength(80) },
+                new ColumnDefinition { Width = new GridLength(80) },
+                new ColumnDefinition { Width = new GridLength(80) },
+                new ColumnDefinition { Width = new GridLength(80) }
             }
             };
 
             // Row 0
             for (int i = 0; i < 4; i++)
             {
-                for (int j = 0; j < 4; i++)
+                for (int j = 0; j < 4; j++)
                 {
-                    grid.Children.Add(new BoxView
-                    {
-                        Color = Color.AliceBlue
-                    }, i , j);
+
+                    var button = new Button();
+                    button.Text = i.ToString();
+                    button.Clicked += OnButtonClicked;
+
+                    grid.Children.Add(button,j,i);
                 }
+
             }
-    
+        
+
 
             Title = "Grid alignment demo";
             Content = grid;
+        }
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button b = (Button)sender;
+            await  b.RelRotateTo(360, 1000);
         }
     }
 }
